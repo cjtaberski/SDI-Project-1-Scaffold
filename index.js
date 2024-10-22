290d98e900943bfb946764ca7d1e828bfb70f512
Connor <connorjtaberski@gmail.com>
2024-10-22 17:57:06 -0400

    Added more events, buttons, and a visited items container, completed some styling

--- index.js
+++ index.js
@@ -1,92 +1,188 @@
 //const axios = require('axios');
-class MuseumExihibit {
-    constructor(id, name, department, year, imageURL) {
-        const itemId = id;
-        const exhibitName = name;
-        const exhibitDepartment = department;
-        const exhibitYear = year;
-        const exhibitImg = imageURL;
-        const listItem = "exhibitItem"
+class MuseumExhibit {
+    constructor(id, name, department, year, imageURL, credit = 'Source unkown') {
+        this.itemId = id;
+        this.exhibitName = name;
+        this.exhibitDepartment = department;
+        this.exhibitYear = year;
+        this.exhibitImg = imageURL;
+        // this.exhibitCredit = credit;
+        if (typeof credit != 'undefined') {
+            this.exhibitCredit = credit;
+        } else {
+            this.exhbitCredit = 'Source unkown'
+        }
+    }
+
+    populateExhibitItem() {
+        if (this.exhibitImg.length > 0 && !(exhibitItems.includes(this.itemId))) {
+            exhibitItems.push(this.itemId);
+            let exhibitContainer = document.getElementById('exhibitItems');
+            let newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'exhibitItem');
+            exhibitContainer.appendChild(newDiv);
+            let itemName = document.createElement('h3');
+            itemName.innerHTML = this.exhibitName;
+            newDiv.appendChild(itemName);
+            let newImg = document.createElement('img');
+            newImg.setAttribute('src', this.exhibitImg);
+            newImg.setAttribute('alt', 'Picture of Exhibit Item');
+            newDiv.appendChild(newImg);
+            let itemDescription = document.createElement('ul');
+            itemDescription.setAttribute('id', 'description');
+            let itemDepartment = document.createElement('li')
+            itemDepartment.textContent = this.exhibitDepartment;
+            itemDescription.appendChild(itemDepartment)
+            let itemYear = document.createElement('li');
+            itemYear.textContent = this.exhibitYear;
+            itemDescription.appendChild(itemYear);
+            let itemCredit = document.createElement('li');
+            itemCredit.textContent = this.exhibitCredit;
+            itemDescription.appendChild(itemCredit);
+            newDiv.appendChild(itemDescription);
+            newDiv.addEventListener('click', () => {
+                this.populateVisitItem()
+            })
+        }
+    }
+
+    populateVisitItem() {
+        if (this.exhibitImg.length > 0 && !(visitItems.includes(this.itemId))) {
+            if (!(visitItems.includes(this.itemId))) {
+                visitItems.push(this.itemId);
+                visitCounter.innerHTML++;
+            }
+            let exhibitContainer = document.getElementById('visitItems');
+            let newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'visitItem');
+            exhibitContainer.appendChild(newDiv);
+            let itemName = document.createElement('h3');
+            itemName.innerHTML = this.exhibitName;
+            newDiv.appendChild(itemName);
+            let newImg = document.createElement('img');
+            newImg.setAttribute('src', this.exhibitImg);
+            newImg.setAttribute('alt', 'Picture of Visit Item');
+            newDiv.appendChild(newImg);
+            let itemDescription = document.createElement('ul');
+            itemDescription.setAttribute('id', 'description');
+            let itemDepartment = document.createElement('li')
+            itemDepartment.textContent = this.exhibitDepartment;
+            itemDescription.appendChild(itemDepartment)
+            let itemYear = document.createElement('li');
+            itemYear.textContent = this.exhibitYear;
+            itemDescription.appendChild(itemYear);
+            let itemCredit = document.createElement('li');
+            itemCredit.textContent = this.exhibitCredit;
+            itemDescription.appendChild(itemCredit);
+            newDiv.appendChild(itemDescription);
+            let visitButton = document.createElement('button')
+            visitButton.setAttribute('type', 'submit');
+            visitButton.textContent = 'Visited';
+            visitButton.addEventListener('click', () => {
+                newDiv.remove()
+                this.populateVisitedItem()
+                visitCounter.innerHTML--;
+            })
+            newDiv.appendChild(visitButton);
+            let removeButton = document.createElement('button')
+            removeButton.setAttribute('type', 'submit');
+            removeButton.textContent = 'Remove';
+            removeButton.addEventListener('click', () => {
+                visitItems.pop(this.itemId)
+                newDiv.remove()
+                visitCounter.innerHTML--;
+            })
+            newDiv.appendChild(removeButton);
+           
+        }
+    }
+
+    populateVisitedItem() {
+        if (this.exhibitImg.length > 0 && !(visitedItems.includes(this.itemId))) {
+            if (!(visitedItems.includes(this.itemId))) {
+                visitedItems.push(this.itemId);
+                visitedCounter.innerHTML++;
+            }
+            let visitedContainer = document.getElementById('visitedItems');
+            let visitedDiv = document.createElement('div');
+            visitedDiv.setAttribute('class', 'exhibitItem');
+            visitedContainer.appendChild(visitedDiv);
+            let itemName = document.createElement('h3');
+            itemName.innerHTML = this.exhibitName;
+            visitedDiv.appendChild(itemName);
+            let newImg = document.createElement('img');
+            newImg.setAttribute('src', this.exhibitImg);
+            newImg.setAttribute('alt', 'Picture of Visit Item');
+            visitedDiv.appendChild(newImg);
+            let itemDescription = document.createElement('ul');
+            itemDescription.setAttribute('id', 'description');
+            let itemDepartment = document.createElement('li')
+            itemDepartment.textContent = this.exhibitDepartment;
+            itemDescription.appendChild(itemDepartment)
+            let itemYear = document.createElement('li');
+            itemYear.textContent = this.exhibitYear;
+            itemDescription.appendChild(itemYear);
+            let itemCredit = document.createElement('li');
+            itemCredit.textContent = this.exhibitCredit;
+            itemDescription.appendChild(itemCredit);
+            visitedDiv.appendChild(itemDescription);
+            let visitedButton = document.createElement('button')
+            visitedButton.setAttribute('type', 'submit');
+            visitedButton.textContent = 'Remove';
+            visitedDiv.appendChild(visitedButton);
+            visitedButton.addEventListener('click', () => {
+                visitedItems.pop(this.itemId)
+                visitedDiv.remove()
+                visitedCounter.innerHTML--;
+            })
+
+        }
     }
 }
 
-fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/111`)
-.then(rawData => rawData.json())
-.then(data => {
-    console.log(data);
-    console.log(data.objectID, data.title, data.primaryImageSmall, data.objectEndDate);
-    const newExhibitItem = new MuseumExihibit(data.objectID, data.title, data.department, data.objectEndDate, data.primaryImageSmall)
-    console.log(newExhibitItem);
-    })
+let newExhibitItem;
+let exhibitItems = [];
+let visitItems = [];
+let visitedItems = [];
+let visitCounter = document.getElementById('visitCounter')
+visitCounter.innerHTML = 0;
+let visitedCounter = document.getElementById('visitedCounter')
+visitedCounter.innerHTML = 0;
 
 function getObjectIds() {
-    var objectIdArray;
-fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
+
+    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
+        .then(rawData => rawData.json())
+        .then(data => data.objectIDs)
+        .then(ids => ids.slice(0, 50))
+        .then(firstIds => firstIds.forEach(index => createExhibitObject(index)))
+}
+
+function createExhibitObject(index) {
+    let newExhibitItem;
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${index}`)
     .then(rawData => rawData.json())
-    .then(data => data.objectIDs)
-    .then(ids => ids.slice(0, 50))
-    .then(firstIds => {
-        firstIds.forEach(index => {
-            let itemNumber = 1;
-            fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${index}`)
-            .then(newData => newData.json())
-            .then(jsonData => createExhibitItem(jsonData))
+    .then(data => {
+        //console.log(data);
+        //console.log(data.objectID, data.title, data.primaryImageSmall, data.objectEndDate);
+        newExhibitItem = new MuseumExhibit(data.objectID, data.title, data.department, data.objectEndDate, data.primaryImageSmall)
+        newExhibitItem.populateExhibitItem();
         })
-    })
 }
-
-function createExhibitItem(apiData) {
-    if (apiData.primaryImageSmall.length > 0) {
-        let exhibitContainer = document.getElementById('exhibitItems');
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'exhibitItem');
-        //newDiv.setAttribute('id', `${itemNumber}`)
-        exhibitContainer.appendChild(newDiv);
-        let itemName = document.createTextNode(`${apiData.title}`)
-        newDiv.appendChild(itemName)
-        let newImg = document.createElement('img');
-        newImg.setAttribute('src', apiData.primaryImageSmall);
-        newImg.setAttribute('alt', 'Picture of Exhibit Item');
-        newDiv.addEventListener('click', () => {
-            let visitContainer = document.getElementById('visitItems');
-            let newerDiv = document.createElement('div');
-            newerDiv.setAttribute('class', 'visitItem');
-            visitContainer.appendChild(newDiv);
-            newerDiv.innerHTML = newDiv.innerHTML;
-            let newerImg = document.createElement('img');
-            newerImg.setAttribute('src', apiData.primaryImageSmall)
-            newerImg.setAttribute('alt', 'Picture of Exhibit Item')
-            newerDiv.appendChild(newerImg)
-            newerDiv.addEventListener('click', () => {
-                newerDiv.remove();
-            })
-        })
-        newDiv.appendChild(newImg);
-        //itemNumber++;
-    }
-}
-
-    // var exhibitItems = document.getElementsByClassName('exhibitItem');
-    // console.log(exhibitItems);
-
-    // for (let i = 0; i < exhibitItems.length; i++) {
-    // console.log(exhibitItems[i]);
-    // exhibitItems[i].addEventListener('click', () => {
-    //     console.log('ExhibitItem was clicked');
-    //     if (exhibitItems[i].id == 'nonListItem') {
-    //         let container = document.getElementsById('visitItems')
-    //         let newDiv = document.createElement('div');
-    //         newDiv.setAttribute('class', 'visitItem');
-    //         newDiv.setAttribute('id', `${exhibitItems[i].id}`);
-    //         container.appendchild(newDiv);
-    //         newDiv.innerHTML = exhibitItems[i].innerHTML;
-    //         let newImg = document.createElement('img')
-    //         let imgSrcDiv = exhibitItems[i].querySelector('img')
-    //         let imgUrl = imgSrcDiv.src;
-    //         console.log(imgUrl);
-    //         newImg.setAttribute
-    //     }
-    //     })
-    // }
 
 getObjectIds()
+
+let searchButton = document.getElementById('searchButton')
+searchButton.addEventListener('click', () => {
+    let searchId = document.getElementById('itemSearch').value;
+    console.log(searchId);
+    createExhibitObject(searchId);
+})
+
+// let visitLink = document.getElementById('visitLink')
+// visitLink.addEventListener('Click', () => {
+//     visitItems.forEach(id => {
+//         createExhibitObject(id);
+//         this.populateVisitItem();
+//     })
+// })
